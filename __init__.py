a46dcb18b209ad3c1676f3b7b191da09d22e1514
NeoEmberArts <authoranimation@gmail.com>
2025-08-28 11:46:12 -0400

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,37 @@
+bl_info = {
+    "name": "Easy Driver",
+    "author": "NeoEmberArts",
+    "version": (1, 0, 0), #MAJOR.MINOR.PATCH
+    "blender": (4, 5, 0),
+    "location": "View3D > Sidebar > Rigging",
+    "description": "Technical rigging made easier! Map transformations from a bone or obect to a custom pose/toggle/value/shapekey via automatically created drivers; Auto clamped and automatic mapping. Auto detects axis of change and the mimimum and maximum values.",
+    "category": "Rigging",
+    "tags": ["Rigging", "Animation", "Drivers"],
+}
+
+import bpy
+
+from . import classes
+from . import ui
+
+def register():
+    classes.register()
+    ui.register()
+    
+    # Scene properties
+    bpy.types.Scene.driver_recorder_props = bpy.props.PointerProperty(type=classes.DriverRecorderProperties)
+    bpy.types.Scene.show_source = bpy.props.BoolProperty(default=True)
+    bpy.types.Scene.show_targets = bpy.props.BoolProperty(default=True)
+
+def unregister():
+    ui.unregister()
+    classes.unregister()
+    if hasattr(bpy.types.Scene, 'driver_recorder_props'):
+        del bpy.types.Scene.driver_recorder_props
+    if hasattr(bpy.types.Scene, 'show_source'):
+        del bpy.types.Scene.show_source
+    if hasattr(bpy.types.Scene, 'show_targets'):
+        del bpy.types.Scene.show_targets
+
+if __name__ == "__main__":
+    register()
