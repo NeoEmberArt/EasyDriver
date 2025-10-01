2dad43d8bb0f1c7f96c13737a18b493c7d96950e
NeoEmberArts <authoranimation@gmail.com>
2025-10-01 10:18:07 -0400

    Updated Version code

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "Easy Driver",
     "author": "NeoEmberArts",
-    "version": (1, 4, 0), #MAJOR.MINOR.PATCH 09/22/2025
+    "version": (1, 5, 0), #MAJOR.MINOR.PATCH 09/31/2025
     "blender": (4, 2, 0),
     "location": "View3D > Sidebar > Rigging",
     "description": "Technical rigging made easier! Map transformations from a bone or obect to a custom pose/toggle/value/shapekey via automatically created drivers; Auto clamped and automatic mapping. Auto detects axis of change and the mimimum and maximum values.",
